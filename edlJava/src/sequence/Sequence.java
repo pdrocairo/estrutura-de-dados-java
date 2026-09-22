badda9714e153ab843220ed055e7c366f941357a
pdrocairo <pedro.cairo21@gmail.com>
2026-09-22 11:12:27 -0300

    adicionado mais alguns metodos a sequence.java

--- edlJava/src/sequence/Sequence.java
+++ edlJava/src/sequence/Sequence.java
@@ -1,7 +1,7 @@
 package sequence;
 
 public class Sequence<T> implements SequenceInterface<T>{
-	public class Slot<T> implements Position<T>{
+	public class Slot implements Position<T>{
 		T value;
 		int index;
 		
@@ -16,7 +16,7 @@ public class Sequence<T> implements SequenceInterface<T>{
 	private int size_;
 	
 	public Sequence(int length, int growth) {
-		sequence = new Slot[length];
+		sequence = (Slot[])new Object[length];
 		this.size_ = 0;
 		this.length = length;
 		growthValue = growth;
@@ -32,7 +32,7 @@ public class Sequence<T> implements SequenceInterface<T>{
 			}else {
 				this.length+=growthValue;
 			}
-			Slot[] newSequence = new Slot[this.length];
+			Slot[] newSequence = (Slot[]) new Object[this.length];
 			for (int i=0;i<this.size_;i++) {
 				newSequence[i] = sequence[i];
 			}
@@ -77,4 +77,80 @@ public class Sequence<T> implements SequenceInterface<T>{
 		}
 		return sequence[this.size_-1];
 	}
+	
+	public boolean isFirst(Position<T> position) {
+		if (isEmpty()) {
+			throw new IllegalStateException("sequencia esta vazia");
+		}
+		Slot slot= checkPosition(position);
+		if (slot.index == 0) {
+			return true;
+		}return false;
+	}
+	
+	public boolean isLast(Position<T> position) {
+		if (isEmpty()) {
+			throw new IllegalStateException("sequencia esta vazia");
+		}
+		Slot slot= checkPosition(position);
+		if (slot.index == this.size_-1) {
+			return true;
+		}return false;
+	}
+	
+	public Position<T> before(Position<T> position){
+		if (isEmpty()) {
+			throw new IllegalStateException("sequencia esta vazia");
+		}
+		if (isFirst(position)) {
+			throw new IllegalStateException("nao existe posicao anterior a informada");
+		}
+		Slot slot = checkPosition(position);
+		return sequence[slot.index-1];
+		
+		
+	}
+	
+    public Position<T> after(Position<T> position){
+    	if (isEmpty()) {
+			throw new IllegalStateException("sequencia esta vazia");
+		}
+    	if (isLast(position)) {
+			throw new IllegalStateException("nao existe posicao apos a que foi informada");
+		}
+    	Slot slot = checkPosition(position);
+    	return sequence[slot.index+1];
+    }
+    
+    public int rankOf(Position<T> position) {
+    	if (isEmpty()) {
+			throw new IllegalStateException("sequencia esta vazia");
+		}
+    	Slot slot = checkPosition(position);
+    	return slot.index;
+    }
+    
+    public Position<T> atRank(int idx){
+    	if (idx < 0 || idx > this.size_) {
+    		throw new IllegalStateException("fora do range");
+    	}
+    	if (isEmpty()) {
+			throw new IllegalStateException("sequencia esta vazia");
+		}
+    	return sequence[idx];
+    }
+    
+    public T replaceAtRank(int idx, T value) {
+    	if (idx < 0 || idx > this.size_) {
+    		throw new IllegalStateException("fora do range");
+    	}
+    	if (isEmpty()) {
+			throw new IllegalStateException("sequencia esta vazia");
+		}
+    	Slot slot = sequence[idx];
+    	T toRemove = slot.value;
+    	slot.value = value;
+    	
+    	return toRemove;
+    }
 }
